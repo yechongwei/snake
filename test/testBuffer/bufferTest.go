41d773afea365101bb9acaeee38505d7f6116810
bibi0922 <62786785+bibi0922@users.noreply.github.com>
2020-03-28 19:50:37 +0800

    Update bufferTest.go

--- test/testBuffer/bufferTest.go
+++ test/testBuffer/bufferTest.go
@@ -1,1 +1,55 @@
 
+package main
+
+import (
+	"flag"
+	"snakeProject/src/buffer"
+	"time"
+
+	"github.com/golang/glog"
+)
+
+func init() {
+	glog.MaxSize = 1024 * 1024 * 100    //最大100M
+	flag.Set("alsologtostderr", "true") // 日志写入文件的同时，输出到stderr
+	flag.Set("log_dir", "./log")        // 日志文件保存目录
+	flag.Set("v", "1")                  // 配置V输出的等级。
+	flag.Parse()
+}
+
+func main() {
+	pool, err := buffer.NewPool(10, 4096)
+	if pool == nil {
+		glog.Info(err)
+		return
+	}
+
+	for i := 0; i < 1; i++ {
+		go func() {
+			glog.Info("Put Data begin..............")
+			for i := 0; i < 51200; i++ {
+				pool.Put(i)
+			}
+			glog.Info("Put Data End:", pool)
+		}()
+	}
+
+	for i := 0; i < 1; i++ {
+		go func() {
+			glog.Info("Get Data begin..............")
+			ticker := time.NewTicker(time.Second * 150)
+			defer ticker.Stop()
+			for {
+				select {
+				case <-ticker.C:
+					glog.Info("Get Data End:", pool)
+					return
+				default:
+					pool.Get()
+				}
+			}
+		}()
+	}
+
+	select {}
+}
